bf0e2f7c634c7e8f15ed3eb4554e4a18b3216849
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-13 21:33:54 -0700

    added increment functionality for cart items

--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -1,7 +1,7 @@
 import CartActionTypes from './cart-types';
-import { addItemToCart } from './cart-utils';
+import { addItemToCart, removeItemFromCart } from './cart-utils';
 
-const INITIAL_STATE = {
+const INITIAL_STATE = { 
   hidden: true,
   cartItems: []
 };
@@ -17,6 +17,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
       };
     default:
       return state;
