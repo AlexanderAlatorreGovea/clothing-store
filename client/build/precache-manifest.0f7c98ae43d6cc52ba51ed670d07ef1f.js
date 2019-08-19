a170d05515af9f05f2158d6f0a16135164e7f3fb
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-18 20:25:27 -0700

    added user authentication, routing with express, sign in and up functionalities, connected the app to the backend, and finally payment options!

---
+++ client/build/precache-manifest.0f7c98ae43d6cc52ba51ed670d07ef1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2a746d8551b9f853840977e235f668",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9e9e159e8f868e0bb3c",
+    "url": "/static/css/main.dbdc250f.chunk.css"
+  },
+  {
+    "revision": "4a0dd78e5dedf0e24f6b",
+    "url": "/static/js/2.862fc4e9.chunk.js"
+  },
+  {
+    "revision": "f9e9e159e8f868e0bb3c",
+    "url": "/static/js/main.c3244824.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
