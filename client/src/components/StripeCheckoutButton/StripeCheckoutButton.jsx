2ba8d7d1380c3c00cb93ad3991f3efef9448d5be
Alexander Govea <alexander_alatorre1993@hotmail.com>
2019-08-18 20:30:01 -0700

    --

--- client/src/components/StripeCheckoutButton/StripeCheckoutButton.jsx
+++ client/src/components/StripeCheckoutButton/StripeCheckoutButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios'
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
